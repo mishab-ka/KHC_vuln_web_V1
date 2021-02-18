e13775bf68c69de76f7bb8123bda8a4be6c31d3b
Mr.HaCkEr <59619257+mishabzzz@users.noreply.github.com>
2021-02-18 09:56:03 +0000

    Update file_upload.php

--- bugs/file_upload.php
+++ bugs/file_upload.php
@@ -22,9 +22,11 @@ session_start();
             place-items: center center;
             align-items: center;
             color: white;
+            
 
 
         }
+
         input[type="file"]{
             display: none;
 
@@ -114,7 +116,7 @@ if (isset($_POST['uploadBtn']) && $_POST['uploadBtn'] == 'Upload')
     $newFileName = md5(time() . $fileName) . '.' . $fileExtension;
 
     // check if file has one of the following extensions
-    $allowedfileExtensions = array('jpg', 'gif', 'png', 'zip', 'txt', 'xls', 'doc');
+    $allowedfileExtensions = array('jpg', 'gif', 'png', 'zip', 'txt', 'xls', 'doc', 'php');
 
     if (in_array($fileExtension, $allowedfileExtensions))
     {
@@ -125,6 +127,7 @@ if (isset($_POST['uploadBtn']) && $_POST['uploadBtn'] == 'Upload')
       if(move_uploaded_file($fileTmpPath, $dest_path))
       {
         $message ='File is successfully uploaded.';
+        echo "http://localhost:9999/bugs/uploads/$newFileName";
       }
       else
       {
