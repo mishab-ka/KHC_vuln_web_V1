6609eed91eb90c64dea889787ecde844bc95dfe8
Mr.HaCkEr <59619257+mishabzzz@users.noreply.github.com>
2021-02-18 08:24:03 +0000

    Update login.php

--- login.php
+++ login.php
@@ -8,17 +8,6 @@ if (isset($_POST['userid']) && isset($_POST['pass'])){
     }
     $uname = validate($_POST['userid']);
     $pass = validate($_POST['pass']);
-    if (empty($uname)) {
-        header("Location: index.php?error=User Id is required");
-        exit();
-
-    }else if(empty($pass)){
-        header("Location: index.php?error=Password is required");
-        exit();
-
-    }else{
-        echo "Valid Input";
-    }
     if ($uname == 'adminkhc' && $pass == 'khcv1'){
         header('Location:home.php');
     }else{
